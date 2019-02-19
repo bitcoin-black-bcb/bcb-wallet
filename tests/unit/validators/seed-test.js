ee38945b2586d36b54d118355a61d7265cadb767
emanherawy <eman.herawy@gmail.com>
2019-02-19 23:12:38 +0200

    add windows bcb-node & generate linux app
    Former-commit-id: 023fbcf9b3ba65def5418c88744f8ebb247d1f16

--- tests/unit/validators/seed-test.js
+++ tests/unit/validators/seed-test.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import validateSeed from '@nano-wallet-company/nano-wallet-desktop/validators/seed';
+import validateSeed from 'bcb-wallet/validators/seed';
 
 describe('Unit | Validator | seed', () => {
   it('passes valid seed', () => {
