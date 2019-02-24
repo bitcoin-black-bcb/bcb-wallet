44659e4c757c53bb8391381404bc0098640de43f
emanherawy <eman.herawy@gmail.com>
2019-02-24 16:50:33 +0200

    fix linting issue
    Former-commit-id: ec788bc9a134b078fa3fa7fed2a19d4ac4ebeb77

--- app/rpc/service.js
+++ app/rpc/service.js
@@ -95,8 +95,10 @@ export default class RPCService extends Service {
 
   async call(action, params = {}) {
     const data = assign({ action }, params);
+    console.log(data, 'data');
 
     const resp = await this.get('ajax').post('/', { data });
+    console.log(resp, 'resp');
 
     if (typeof resp.error === 'string') {
       switch (resp.error) {
@@ -119,6 +121,7 @@ export default class RPCService extends Service {
   }
 
   walletCreate() {
+   console.log('walletCreate');
     return this.call(actions.WALLET_CREATE);
   }
 
